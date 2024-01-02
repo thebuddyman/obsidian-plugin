60b0f34c8e6a5221ef22136711ffd98e30c1b317
Tobias <38814998+thebuddyman@users.noreply.github.com>
2024-01-03 00:54:04 +0100

    delete api key

--- utils/settingsUtil.ts
+++ utils/settingsUtil.ts
@@ -8,10 +8,6 @@ export const DEFAULT_SETTINGS: PluginSettings = {
     apiKey: '' // or an empty string ''
 };
 
-// export const DEFAULT_SETTINGS: PluginSettings = {
-//     apiKey: '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'
-// };
-
 export function loadPluginSettings(plugin: Plugin): Promise<PluginSettings> {
     return plugin.loadData().then(data => {
         return { ...DEFAULT_SETTINGS, ...data };
